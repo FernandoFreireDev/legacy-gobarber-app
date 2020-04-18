d459082ddf211867fbc98ea047620fe581280f11
Fernando Freire <fernando.ricardo.freire@gmail.com>
2020-04-17 22:58:52 -0300

    configurando statusbar

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import SignIn from '~/pages/SignIn';
@@ -8,13 +9,16 @@ const Stack = createStackNavigator();
 
 export default function Routes() {
   return (
-    <Stack.Navigator
-      screenOptions={{
-        headerShown: false,
-      }}
-    >
-      <Stack.Screen name="SignIn" component={SignIn} />
-      <Stack.Screen name="SignUp" component={SignUp} />
-    </Stack.Navigator>
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="SignIn" component={SignIn} />
+        <Stack.Screen name="SignUp" component={SignUp} />
+      </Stack.Navigator>
+    </>
   );
 }
