753a5d4bf26036cc771ccb7a3bd7a44a9a6b0ba8
Fernando Freire <fernando.ricardo.freire@gmail.com>
2020-04-18 00:01:53 -0300

    configurando redux

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import '~/config/ReactotronConfig';
 
+import { store, persistor } from '~/store';
 import Routes from '~/routes';
 
 export default function src() {
   return (
-    <NavigationContainer>
-      <Routes />
-    </NavigationContainer>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <NavigationContainer>
+          <Routes />
+        </NavigationContainer>
+      </PersistGate>
+    </Provider>
   );
 }
