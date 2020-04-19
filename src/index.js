112d8ab35a657c8671d5348212fbfa9f36ca5d9e
Fernando Freire <fernando.ricardo.freire@gmail.com>
2020-04-19 11:30:14 -0300

    rota inicial

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import 'react-native-gesture-handler';
 import React from 'react';
+import { StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
@@ -7,13 +8,15 @@ import { PersistGate } from 'redux-persist/integration/react';
 import '~/config/ReactotronConfig';
 
 import { store, persistor } from '~/store';
+
 import Routes from '~/routes';
 
-export default function src() {
+export default function Index() {
   return (
     <Provider store={store}>
       <PersistGate persistor={persistor}>
         <NavigationContainer>
+          <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
           <Routes />
         </NavigationContainer>
       </PersistGate>
--- src/routes.js
+++ src/routes.js
@@ -1,24 +1,40 @@
 import React from 'react';
-import { StatusBar } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { useSelector } from 'react-redux';
 
 import SignIn from '~/pages/SignIn';
 import SignUp from '~/pages/SignUp';
+import Dashboard from '~/pages/Dashboard';
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function App() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Dashboard" component={Dashboard} />
+    </Tab.Navigator>
+  );
+}
 
 export default function Routes() {
+  const isSigned = useSelector((state) => state.auth.signed);
+
   return (
-    <>
-      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
-      <Stack.Navigator
-        screenOptions={{
-          headerShown: false,
-        }}
-      >
-        <Stack.Screen name="SignIn" component={SignIn} />
-        <Stack.Screen name="SignUp" component={SignUp} />
-      </Stack.Navigator>
-    </>
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      {!isSigned ? (
+        <>
+          <Stack.Screen name="SignIn" component={SignIn} />
+          <Stack.Screen name="SignUp" component={SignUp} />
+        </>
+      ) : (
+        <Stack.Screen name="App" component={App} />
+      )}
+    </Stack.Navigator>
   );
 }
